eca0d88ab84b39ef81afe58e254394fc1942bca2
unknown <alfredomarenco@boletea.com>
2022-10-14 09:21:40 -0500

    Avances del sistema

--- app/src/main/java/com/example/controllaccess/MainActivity.java
+++ app/src/main/java/com/example/controllaccess/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.controllaccess;
 
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.InputType;
 import android.util.Log;
-import android.util.LogPrinter;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,8 +18,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
@@ -36,14 +31,12 @@ import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.controllaccess.database.DataBaseCodes;
-import com.example.controllaccess.database.DataBaseHelper;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.security.Key;
-import java.sql.SQLOutput;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -52,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     Button buttonSend;
     TextView textBarcode;
     RelativeLayout bgLayout;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,11 +57,79 @@ public class MainActivity extends AppCompatActivity {
         textBarcode = (TextView) findViewById(R.id.textBarcode);
         bgLayout = findViewById(R.id.bgLayout);
         textBarcode.setInputType(InputType.TYPE_NULL);
+        mp = MediaPlayer.create(MainActivity.this,R.raw.ok);
+
+        RequestQueue requestQueue;
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024000 * 1024000); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        requestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        requestQueue.start();
+
+        String url = "https://controllaccess.boletea.com/api/code";
+        StringRequest codeRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    Log.d("Conection","Si me conecte");
+                    //Log.d("Objeto",""+jsonArray);
+                    for (int i = 0; i <= jsonArray.length();i++){
+                        DataBaseCodes dataBaseCodes =  new DataBaseCodes(MainActivity.this);
+                        String barcode = jsonArray.getJSONObject(i).getString("barcode").toString();
+                        String name = jsonArray.getJSONObject(i).getString("name").toString();
+                        String section = jsonArray.getJSONObject(i).getString("section").toString();
+                        String price_category = jsonArray.getJSONObject(i).getString("price_category").toString();
+                        String row = jsonArray.getJSONObject(i).getString("row").toString();
+                        String seat = jsonArray.getJSONObject(i).getString("seat").toString();
+                        String amount = jsonArray.getJSONObject(i).getString("amount").toString();
+                        String order = jsonArray.getJSONObject(i).getString("order").toString();
+                        String sales_channel = jsonArray.getJSONObject(i).getString("sales_channel").toString();
+                        String ext = jsonArray.getJSONObject(i).getString("ext").toString();
+                        String status = jsonArray.getJSONObject(i).getString("status").toString();
+                        String event_id = jsonArray.getJSONObject(i).getString("event_id").toString();
+
+                        long id = dataBaseCodes.insertCode(barcode,name,section,price_category,row,seat,amount,order,sales_channel,ext,status,event_id);
+
+                        if (id > 0){
+                            //Toast.makeText(MainActivity.this,"Se cargo la base de datos",Toast.LENGTH_LONG).show();
+                            textBarcode.setText("Se cargo la base de datos");
+                        }else{
+                            textBarcode.setText("No se cargo la base de datos");
+                        }
+                    }
+
+                    Toast.makeText(MainActivity.this,"Todos los codigos Obtenidos",Toast.LENGTH_LONG).show();
+                }catch (JSONException ex){
+                    Log.d("Error:",ex.toString());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Conection",error.toString());
+            }
+        });
+        requestQueue.add(codeRequest);
 
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addElementsDataBase();
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                intentIntegrator.setPrompt("Lector");
+                intentIntegrator.setCameraId(0);
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setBarcodeImageEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.initiateScan();
             }
         });
 
@@ -106,7 +168,9 @@ public class MainActivity extends AppCompatActivity {
                     if (status == 1){
                         bgLayout.setBackgroundResource(R.color.green);
                         textBarcode.setTextColor(Color.rgb(255, 255, 255));
+                        textBarcode.setTextSize(60);
                         textBarcode.setText("PASE");
+                        mp.start();
 
                     }else if(status == 0){
                         bgLayout.setBackgroundResource(R.color.red);
@@ -121,47 +185,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 textBarcode.setText("Codigo no valid");
+                bgLayout.setBackgroundResource(R.color.red);
+                textBarcode.setTextColor(Color.rgb(255, 255, 255));
 
             }
         });
         Volley.newRequestQueue(this).add(codeRequest);
     }
 
-    private void addElementsDataBase(){
-        RequestQueue requestQueue;
-
-        // Instantiate the cache
-        Cache cache = new DiskBasedCache(getCacheDir(), 1024000 * 1024000); // 1MB cap
-
-        // Set up the network to use HttpURLConnection as the HTTP client.
-        Network network = new BasicNetwork(new HurlStack());
-
-        // Instantiate the RequestQueue with the cache and network.
-        requestQueue = new RequestQueue(cache, network);
-
-        // Start the queue
-        requestQueue.start();
-
-        String url = "https://controllaccess.boletea.com/api/code";
-        StringRequest codeRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONArray jsonObject = new JSONArray(response);
-                    Log.d("Conection","Si me conecte");
-                    //Log.d("Objeto",""+jsonObject);
-                    textBarcode.setText(""+jsonObject.length());
-                    Toast.makeText(MainActivity.this,"Todos los codigos Obtenidos",Toast.LENGTH_LONG).show();
-                }catch (JSONException ex){
-                    Log.d("Error:",ex.toString());
-                }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result != null){
+            if (result.getContents() == null){
+                Toast.makeText(MainActivity.this,"Lector cancelado",Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(MainActivity.this,"Lector funciona",Toast.LENGTH_LONG).show();
+                showCode(result.getContents());
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("Conection",error.toString());
-            }
-        });
-        requestQueue.add(codeRequest);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
